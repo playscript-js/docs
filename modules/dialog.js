d5453af3a66bd0062e2ac74a4fcb146bd60306d2
playscript-js <dealdreygaroeb@gmail.com>
2021-01-21 05:47:12 +0200

    upd

--- modules/dialog.js
+++ modules/dialog.js
@@ -1,6 +1,6 @@
 class dialog extends modal{
   constructor(title,body,call,opt,okb){
-    
+    super(null,"show")
     var obj,objTitle,objBody,objButtonlay, objButton,ob2, objBase;
     if(title)objTitle=new text(title,"100%",null,"indent").css({color:"var(--accent)","font-size":"22px","box-shadow":"0px 0px 5px 3px var(--shadow)"})
     objBody=new flex("column","100%").css({"max-height":window.innerHeight-250+"px",overflow:"scroll"}).add(body||"")
@@ -9,11 +9,11 @@ class dialog extends modal{
     objButtonlay=new flex("row","100%").add(objButton, ob2)
     objBase=new flex("column","80%").css({"background":"var(--back)","border-radius":"8px","box-shadow":"0px 3px 10px 3px var(--shadow)"}).add(objTitle||"",objBody,objButtonlay)
     
-    super(objBase,"show")
+   // super(objBase,"show")
     this.call=call||false
     objButton.on("click",this.fclose.bind(this))
     ob2.on("click",this.tclose.bind(this))
-
+    this.add(objBase)
     return this
   }
   tclose(){this.close();if(this.call)this.call(true)}
--- play.js
+++ play.js
@@ -123,6 +123,7 @@ class setup {
     var obj,objTitle,objBody,objButtonlay, objButton,ob2, objBase;
     if(title)objTitle=new text(title,"100%",null,"indent").css({color:"var(--accent)","font-size":"22px","box-shadow":"0px 0px 5px 3px var(--shadow)"})
     objBody=new edit(body,"90%").css({"padding-left":"5%","max-height":window.innerHeight-200+"px"})
+    objBody.raw.autofocus=true
     if(opt.includes("number"))objBody.attr("type","number")
     objButton=new button("cancel",null, null, "silent").css("margin-left","auto").on("click",function(){obj.close();if(typeof call=="function")call()})
     ob2=new button("submit", null, null, "silent").on("click",function(){obj.close();if(typeof call=="function")call(objBody.text())})
